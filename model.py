b5ed212a6ba4ccac51b676a7d2ce40e98fb38c1d
manip_ego <52987267+manipEgo@users.noreply.github.com>
2022-12-22 20:56:38 +0800

    feat: deeper classifier model

--- model.py
+++ model.py
@@ -5,10 +5,21 @@ from src import  N_CLASSES, FEATURE_DIM
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.hidden = nn.Linear(FEATURE_DIM, FEATURE_DIM)
-        self.out = nn.Linear(FEATURE_DIM, N_CLASSES)
+        self.layer_1 = nn.Linear(FEATURE_DIM, int(FEATURE_DIM / 2)) 
+        self.layer_2 = nn.Linear(int(FEATURE_DIM / 2), int(FEATURE_DIM / 4))
+        self.layer_out = nn.Linear(int(FEATURE_DIM / 4), N_CLASSES) 
 
-    def forward(self, x):
-        x = torch.sigmoid(self.hidden(x))
-        x = self.out(x)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.1)
+        self.batchnorm1 = nn.BatchNorm1d(int(FEATURE_DIM / 2))
+        self.batchnorm2 = nn.BatchNorm1d(int(FEATURE_DIM / 4))
+
+    def forward(self, inputs):
+        x = self.relu(self.layer_1(inputs))
+        x = self.batchnorm1(x)
+        x = self.relu(self.layer_2(x))
+        x = self.batchnorm2(x)
+        x = self.dropout(x)
+        x = self.layer_out(x)
+
         return x
