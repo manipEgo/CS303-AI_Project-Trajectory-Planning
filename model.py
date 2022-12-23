1c6d69654c21acee73ff8b76eaa871b82b1f63a0
manip_ego <52987267+manipEgo@users.noreply.github.com>
2022-12-23 12:43:22 +0800

    feat: new classification model

--- model.py
+++ model.py
@@ -7,18 +7,22 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.layer_1 = nn.Linear(FEATURE_DIM, int(FEATURE_DIM / 2)) 
         self.layer_2 = nn.Linear(int(FEATURE_DIM / 2), int(FEATURE_DIM / 4))
-        self.layer_out = nn.Linear(int(FEATURE_DIM / 4), N_CLASSES) 
+        self.layer_3 = nn.Linear(int(FEATURE_DIM / 4), int(FEATURE_DIM / 8))
+        self.layer_out = nn.Linear(int(FEATURE_DIM / 8), N_CLASSES) 
 
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(p=0.1)
         self.batchnorm1 = nn.BatchNorm1d(int(FEATURE_DIM / 2))
         self.batchnorm2 = nn.BatchNorm1d(int(FEATURE_DIM / 4))
+        self.batchnorm3 = nn.BatchNorm1d(int(FEATURE_DIM / 8))
 
     def forward(self, inputs):
         x = self.relu(self.layer_1(inputs))
         x = self.batchnorm1(x)
         x = self.relu(self.layer_2(x))
         x = self.batchnorm2(x)
+        x = self.relu(self.layer_3(x))
+        x = self.batchnorm3(x)
         x = self.dropout(x)
         x = self.layer_out(x)
 
