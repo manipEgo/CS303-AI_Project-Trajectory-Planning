42d0a6b00672d97f863437bdde74d91b526843e4
manip_ego <52987267+manipEgo@users.noreply.github.com>
2022-12-28 23:06:41 +0800

    feat: new model

--- model.py
+++ model.py
@@ -1,23 +1,17 @@
-import torch.cuda
 import torch.nn as nn
-from src import  N_CLASSES, FEATURE_DIM
 
 class Net(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
-        self.layer_1 = nn.Linear(FEATURE_DIM, int(FEATURE_DIM / 2)) 
-        self.layer_2 = nn.Linear(int(FEATURE_DIM / 2), int(FEATURE_DIM / 4))
-        self.layer_out = nn.Linear(int(FEATURE_DIM / 4), N_CLASSES) 
-
-        self.relu = nn.ReLU()
-        self.batchnorm1 = nn.BatchNorm1d(int(FEATURE_DIM / 2))
-        self.batchnorm2 = nn.BatchNorm1d(int(FEATURE_DIM / 4))
-
-    def forward(self, inputs):
-        x = self.relu(self.layer_1(inputs))
-        x = self.batchnorm1(x)
-        x = self.relu(self.layer_2(x))
-        x = self.batchnorm2(x)
-        x = self.layer_out(x)
-
-        return x
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.BatchNorm1d(128),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.BatchNorm1d(64),
+            nn.Linear(64, 10)
+        )
+    
+    def forward(self, x):
+        return self.model(x)
